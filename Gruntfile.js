71535e35d254780e0428b953eb395aea25f1a74d
Max Nachlinger <max.nachlinger@gmail.com>
2013-09-03 22:19:44 -0700

    Refactor.

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,52 +15,16 @@ module.exports = function (grunt) {
 		},
 
 		node_tap: {
-			failures_console: {
-				options: {
-					outputType: 'failures', // tap, failures, stats
-					outputTo: 'console' // or file
-					//outputFilePath: '/tmp/out.log' // path for output file, only makes sense with outputTo 'file'
-				},
-				files: {
-					'tests': ['./test/data/mixed.js']
-				}
-			},
-			stats_console: {
-				options: {
-					outputType: 'stats',
-					outputTo: 'console'
-				},
-				files: {
-					'tests': ['./test/data/mixed.js']
-				}
-			},
-			tap_stream_console: {
-				options: {
-					outputType: 'tap',
-					outputTo: 'console'
-				},
-				files: {
-					'tests': ['./test/data/mixed.js']
-				}
-			},
-			tap_stream_file: {
-				options: {
-					outputType: 'tap',
-					outputTo: 'file',
-					outputFilePath: '/tmp/out.log'
-				},
-				files: {
-					'tests': ['./test/data/mixed.js']
-				}
-			},
 			test: {
 				options: {
-					outputType: 'tap', // tap, failures, stats
+					outputType: 'stats', // tap, failures, stats
 					outputTo: 'console' // or file
 					//outputFilePath: '/tmp/out.log' // path for output file, only makes sense with outputTo 'file'
 				},
 				files: {
-					'tests': ['./test/data/*.js']
+					'tests': [
+						'./test/*.js'
+					]
 				}
 			}
 		}
@@ -68,12 +32,5 @@ module.exports = function (grunt) {
 
 	grunt.loadTasks('tasks');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-
 	grunt.registerTask('default', ['jshint', 'node_tap:test']);
-
-	grunt.registerTask('fc', ['node_tap:failures_console']);
-	grunt.registerTask('sc', ['node_tap:stats_console']);
-	grunt.registerTask('tc', ['node_tap:tap_stream_console']);
-	grunt.registerTask('tf', ['node_tap:tap_stream_file']);
-
 };
--- test/nodeTapTests.js
+++ test/nodeTapTests.js
@@ -1,60 +1,49 @@
 "use strict";
+var util = require('util');
+var path = require('path');
 var test = require('tap').test;
-var grunt = require('grunt');
-/*
-test("Handles failed node-tap tests", function (t) {
-	givenFilePaths([
-		'./data/mixed.js'
-	], function (err, res) {
+var nodeTap = require('../lib/nodeTap.js');
+var consts = require('../lib/consts.js');
+var exitCodes = consts.exitCodes();
+
+test("Handles a file with failing node-tap tests", function (t) {
+	nodeTap({
+		outputType: 'stats',
+		outputTo: 'console',
+		files: [path.resolve(__dirname + '/data/mixed.js')]
+	}, function(err, res) {
 		t.notOk(err, "No error was returned");
-		t.ok("A result was received");
-		t.notOk(res.testsPassed, "Some tests failed.");
-		t.ok(res.failedTests, "Output was returned.");
+		t.ok(res, "A result was received: " + util.inspect(res));
+		t.equal(res.exitCode, exitCodes.testsFailed, "The result indicated that a test failed");
 		t.end();
 	});
 });
 
-test("Handles passed node-tap tests", function (t) {
-	givenFilePaths([
-		'./data/passing.js'
-	], function (err, res) {
+test("Handles a file with passing node-tap tests", function (t) {
+	nodeTap({
+		outputType: 'stats',
+		outputTo: 'console',
+		files: [path.resolve(__dirname + '/data/passing.js')]
+	}, function(err, res) {
 		t.notOk(err, "No error was returned");
-		t.ok("A result was received");
-		t.ok(res.testsPassed, "All tests passed.");
-		t.ok(res.failedTests, "Output was returned.");
+		t.ok(res, "A result was received: " + util.inspect(res));
+		t.equal(res.exitCode, exitCodes.testsPassed, "The result indicated that all tests passed");
 		t.end();
 	});
 });
 
 test("Handles N node-tap test files", function (t) {
-	givenFilePaths([
-		'./data/mixed.js',
-		'./data/passing.js'
-	], function (err, res) {
+	nodeTap({
+		outputType: 'stats',
+		outputTo: 'console',
+		files: [
+			path.resolve(__dirname + '/data/mixed.js'),
+			path.resolve(__dirname + '/data/passing.js')
+		]
+	}, function(err, res) {
 		t.notOk(err, "No error was returned");
-		t.ok("A result was received");
-		t.notOk(res.testsPassed, "Some tests failed.");
-		t.ok(res.failedTests, "Output was returned.");
+		t.ok(res, "A result was received: " + util.inspect(res));
+		t.equal(res.exitCode, exitCodes.testsFailed, "The result indicated that a test failed");
 		t.end();
 	});
 });
-
-function givenFilePaths(paths, cb) {
-	grunt.initConfig({
-		node_tap: {
-			options: {
-				outputType: 'stats',
-				outputTo: 'console'
-			},
-			files: {
-				'tests': paths
-			}
-		}
-	});
-	grunt.registerMultiTask('node_tap', '', require('../tasks/node_tap.js'));
-	grunt.registerTask('tasks_complete', '', cb);
-	grunt.task.run('node_tap', 'tasks_complete');
-	cb();
-}
-*/
-
